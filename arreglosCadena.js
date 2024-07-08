478b33e4ea2d65b9ec89f210a25c5562aa1ec92c
juapew2021 <62611754+juapew2021@users.noreply.github.com>
2024-07-08 03:49:33 -0500

    Update arreglosCadena.js

--- arreglosCadena.js
+++ arreglosCadena.js
@@ -1,10 +1,10 @@
 // Arreglos [Arrays]
 
-var myArreglo = ['Juan', 35];
+/* var myArreglo = ['Juan', 35];
 
-//console.log(myArreglo)
+console.log(myArreglo[1]); */
 
-// Arreglos añadidos
+/* // Arreglos añadidos
 
 var listaEstudiantes = [['juan', 78],['pedro', 76], ['maria', 45]];
 
@@ -41,7 +41,7 @@ Arreglo :         [[1, 2, 3], [2, 4, 6],['ee','rrr','tttt', 'ooo']]
 Indice:                0           1            2    
 Indices Internos :  0  1  2    0  1  2     0     1    2       3
 */
-let multiArreglos = [[1, 2, 3], [2, 4, 6],['ee','rrr','tttt', 'ooo']];
+/* let multiArreglos = [[1, 2, 3], [2, 4, 6],['ee','rrr','tttt', 'ooo']];
 
 console.log(multiArreglos[0]); // [ 1, 2, 3 ]   
 console.log(multiArreglos[1]); // [ 2, 4, 6 ]
@@ -50,4 +50,32 @@ console.log(multiArreglos[2]); // [ 'ee', 'rrr', 'tttt', 'ooo' ]
 // Sacamos los indices internos 
 
 console.log(multiArreglos[0][2]); //3
-console.log(multiArreglos[2][3]); // ooo
+console.log(multiArreglos[2][3]); // ooo */
+ 
+
+
+
+
+// arreglos 
+
+//var listaEstudiantes = [andres, juanDavid, Mateo, JuanP];
+/* let resultado;
+var listaEstudiantes;
+
+function suGrado(edad) {
+    listaEstudiantes = [['juandavid', edad] , ['mateo', edad]];
+    if (edad >= 1 && edad <= 4 ) {
+        resultado = 'El estudiante esta el grado 8 ';
+    }else if (edad >= 4 && edad <= 10 ) {
+    resultado = 'El estudiante esta el grado 9 ';
+    
+     } 
+}
+suGrado(10);
+console.log(resultado); */
+// tipos de datos 
+// int, string , numerico 
+
+var listaPartesCuerpo = [['Juan', 'ojos', 'negros'], ['pedro', 'ojos', 'azules'], ['mateo', 'ojos', 'verdes']]
+
+console.log("El estudiante tienen un color de ojos:", listaPartesCuerpo[2][2]);
