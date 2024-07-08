9b10b554b454f8b0924bade84258e6f0bb4a59db
juandavidsanchezc <116021253+juandavidsanchezc@users.noreply.github.com>
2024-07-07 21:00:14 -0500

    puedeconducir

--- tuPesoIdeal.js
+++ tuPesoIdeal.js
@@ -4,7 +4,7 @@ Si el peso es el Doble de la Edad = El peso es normal
 Si el peso es mas del doble de la edad = Su peso esta sobrepasado 
 Si Su peso es por Debajo del Doble de su edad : Su peso es optimo 
 */
-let resultado;
+/*let resultado;
 var pesoIdeal;
 function diNosTuPeso(edad, altura, peso) {
     pesoIdeal = ((altura - 100) + (edad / 10) * (0.9));
@@ -26,3 +26,41 @@ function diNosTuPeso(edad, altura, peso) {
 diNosTuPeso(38, 174, 77.42);
 console.log(pesoIdeal);
 console.log(resultado);
+*/
+
+
+
+/*let resultado;
+
+function esParOImpar(num) {
+    
+    if (num % 2 === 0) {
+        resultado = "El número es par";
+    } else {
+        resultado = "El número es impar";
+    }
+}
+esParOImpar(862);
+console.log(resultado);
+*/
+
+let resultado;
+
+function puedeConducir(esMayorDeEdad, estaBorracho, superoLimiteVelocidad, tieneLicencia) {
+    
+
+    if (esMayorDeEdad === "sí" && estaBorracho === "no" && superoLimiteVelocidad === "no" && tieneLicencia === "sí") {
+        resultado = "Puede conducir";
+    } else {
+        resultado = "No puede conducir";
+    }
+    return resultado;
+}
+
+let esMayorDeEdad = "si";
+let estaBorracho = "no";
+let superoLimiteVelocidad = "no";
+let tieneLicencia = "si";
+
+console.log(puedeConducir(esMayorDeEdad, estaBorracho, superoLimiteVelocidad, tieneLicencia));
+
