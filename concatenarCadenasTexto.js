3d3425d6b9f3fac6b517fbf9aabe4d220c5f6c04
juandavidsanchezc <116021253+juandavidsanchezc@users.noreply.github.com>
2024-07-10 23:39:49 -0500

    .

--- concatenarCadenasTexto.js
+++ concatenarCadenasTexto.js
@@ -1,12 +1,39 @@
 
 
-var miSustantivo = "Perro";
+/*var miSustantivo = "Perro";
 var miAdjetivo = "negro";
 var miVerbo = "corrío";
-var miAdverbio = "rápidamente"; 
+var miAdverbio = "rápidamente"; */
 
 /* El perro negro corrío rápidamente a la tienda. */
 
-var titulo = "El" + " perro" + " negro" + " corrío"
+/*var titulo = "El" + " perro" + " negro" + " corrío"
 
-console.log(titulo)
+console.log(titulo)*/
+
+
+var miSustantivo = "elefante";
+var miAdjetivo = "gris";
+var miVerbo = "camina";
+var miAdverbio = "lentamente"; 
+
+var titulo1 = "El " + miSustantivo + " " + miAdjetivo + " " + miVerbo + " " + miAdverbio + " por la selva africana"
+
+
+
+var miSustantivo = "jirafa";
+var miAdjetivo = "alta";
+var miVerbo = "juega";
+var miAdverbio = "cuidadosamente"; 
+
+var titulo2 = "la " + miSustantivo + " " + miAdjetivo + " " + miVerbo + "con sus hijos " + miAdverbio
+
+var miSustantivo = "leopardo";
+var miAdjetivo = "fuerte";
+var miVerbo = "corre";
+var miAdverbio = "cuidadosamente"; 
+
+var titulo3 = "El " + miSustantivo + " " + miAdjetivo + " " + miVerbo + "por la selva " + miAdverbio
+
+
+console.log(titulo1:titulo2)
