d23aa3dfcd68963458c7e3402a2bdbe15eb75b8b
juandavidsanchezc <116021253+juandavidsanchezc@users.noreply.github.com>
2024-07-31 21:47:06 -0500

    lavadero
    BIENVENIDO AL LAVADERO MAS AGOGO DEL PÉDASO MI PAPACHO

--- lavadero.js
+++ lavadero.js
@@ -1,35 +1,3 @@
-/*var ordenesDePizza = [
-    {
-
-        "tipo de pizza" : "Margarita",
-        "precio" : 34.89,
-        "tamaño": "Individual",
-        "toppings": [
-            "Sala rosada",
-            "Queso curado",
-            "coca cola"
-         ],
-
-         "llevar" : true
-
-        },
-    
-    {
-        "tipo de pizza" : "Napolitana",
-        "precio" : 31.89,
-        "tamaño": "Familiar",
-        "toppings": [
-            "Sala Negra",
-            "Queso extra",
-            "Agua"
-        ],
-        "llevar" : false
-    }
-];
-console.log("El cliente ha pedido una pizza: " + ordenesDePizza[1]["tipo de pizza"]); 
-console.log("Su precio es: " + ordenesDePizza[1]["precio"]); 
-console.log("Que topping desea llevar: " + ordenesDePizza[1]["toppings"][2]); */
-
 const readlineSync = require('readline-sync');
 
 var serviciosDeLavado = [
